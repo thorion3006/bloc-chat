262f1820cb8b046962b7c006a8deeb4f7d6187dc
Sajeev Ramasamy <thorion3006@gmail.com>
2017-02-23 22:08:40 +0530

    User story 1: Completed

---
+++ app/scripts/controllers/HomeCtrl.js
@@ -0,0 +1,9 @@
+(function() {
+    function HomeCtrl($scope, Room) {
+        $scope.rooms = Room.all;
+    }
+    
+    angular
+        .module('blocChat')
+        .controller('HomeCtrl', ['$scope', 'Room', HomeCtrl]);
+})();
